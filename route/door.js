5a42dccd32fb01ee0cfe7aba64d1c07d20184b73
TMineCola <s104213083@mail1.ncnu.edu.tw>
2018-08-09 16:25:00 +0800

    新增門鎖控制 route

--- route/door.js
+++ route/door.js
@@ -1,24 +1,80 @@
-var express = require('express');
-var router = express.Router();
-
-/* 查詢電源狀態 */
-router.get('/power', function(req, res, next) {
-
-});
-
-/* 電源狀態切換 */
-router.post('/power', function(req, res, next) {
-
-});
+const express = require('express');
+const router = express.Router();
 
 /* 查詢門鎖狀態 */
-router.get('/state', function(req, res, next) {
-
+router.get('/door', function(req, res) {
+    let result = req.door.reload();
+    if (result) {
+        res.status(200).send(result);
+    } else {
+        res.status(500).send({
+            "message": "狀態重新讀取失敗"
+        });
+    }
 });
 
-/* 門鎖狀態切換 */
-router.post('/state', function(req, res, next) {
+/* 修改門鎖狀態 */
+router.post('/door', function(req, res) {
+    let requsetObject = req.body;
+    if(requsetObject['method'] && (requsetObject['method'] != 'open' || requsetObject['method'] != 'close' || requsetObject['method'] != 'temp')) {
+        if(requsetObject['method'] == 'temp') {
+            if(requsetObject['delay'] && typeof(requsetObject['delay']) == 'number') {
+                if(req.door.lockSwitch(requsetObject['method'], requsetObject['delay'])) {
+                    res.status(200).send({
+                        "message": "狀態修改成功"
+                    });
+                } else {
+                    res.status(500).send({
+                        "message": "狀態修改失敗"
+                    });
+                }
+            } else {
+                res.status(500).send({
+                    "message": "缺少延遲秒數欄位或該欄位值不為數字"
+                });
+            }
+        } else {
+            if(req.door.lockSwitch(requsetObject['method'])) {
+                res.status(200).send({
+                    "message": "狀態修改成功"
+                });
+            } else {
+                res.status(500).send({
+                    "message": "狀態修改失敗"
+                });
+            }
+        }
+    } else {
+        res.status(500).send({
+            "message": "缺少狀態欄位或不合法的狀態"
+        });
+    }
+});
 
+/* 啟動電源 */
+router.put('/door', function(req, res) {
+    if(req.door.attach()) {
+        res.status(200).send({
+            "message": "門鎖裝置啟動成功"
+        });
+    } else {
+        res.status(500).send({
+            "message": "門鎖裝置啟動失敗"
+        });
+    }
+});
+
+/* 關閉電源 */
+router.delete('/door', function(req, res) {
+    if(req.door.detach()) {
+        res.status(200).send({
+            "message": "門鎖裝置關閉成功"
+        });
+    } else {
+        res.status(500).send({
+            "message": "門鎖裝置關閉失敗"
+        });
+    }
 });
 
 module.exports = router;
